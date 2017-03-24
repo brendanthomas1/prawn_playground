2867e6d4217de72de02111f602c7b8d8ada22ad7
Brendan Thomas <bt331609@gmail.com>
2017-03-24 15:23:39 -0400

    because blocks

--- app.rb
+++ app.rb
@@ -11,12 +11,18 @@ class PrawnPlayground < Sinatra::Base
     content_type 'application/pdf'
     color = '#000000'
 
+    create_and_render do |pdf|
+      pdf.formatted_text_box(
+        [{ text: 'Hi there!', color: color }],
+           valign: :center, size: 20, height: 30,
+           width: 50, at: [10, 500], overflow: :shrink_to_fit,
+           min_font_size: 8, single_line: true, )
+    end
+  end
+
+  def create_and_render
     pdf = Prawn::Document.new
-    pdf.formatted_text_box(
-      [{ text: 'Hi there!', color: color }],
-         valign: :center, size: 20, height: 30,
-         width: 50, at: [10, 500], overflow: :shrink_to_fit,
-         min_font_size: 8, single_line: true, )
+    yield pdf if block_given?
     pdf.render
   end
 end
